16c7377487557be53db3d0f13444297eb356ab25
Rubi Ben-Zvi <rubi.benzvi@gmail.com>
2023-02-24 09:05:27 +0200

    f

--- app/src/main/java/com/example/Dbtime_Watch/MainActivity5.java
+++ app/src/main/java/com/example/Dbtime_Watch/MainActivity5.java
@@ -16,16 +16,27 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
 import com.example.Dbtime_Wathc.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity5 extends Activity implements SensorEventListener {
 
-    TextView mTextViewHeart;
+    TextView mTextViewHeart,tv2;
     String TAG;
-    String hr="tt";
+    String hr="tt",email;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public FirebaseAuth mAuth;
 
 
     @Override
@@ -34,28 +45,23 @@ public class MainActivity5 extends Activity implements SensorEventListener {
         setContentView(R.layout.activity_main5);
 
         TAG = "test";
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        email = user.getEmail();
+
         mTextViewHeart = (TextView) findViewById(R.id.textView4);
         Button bt = findViewById(R.id.button3);
         bt.setVisibility(View.INVISIBLE);
         (new Handler()).postDelayed(this::showBtn, 12000);
-        TextView tv2 = findViewById(R.id.textView2);
+        tv2 = findViewById(R.id.textView2);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         getHeartRate();
 
         findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                String ms = " נשלח דופק: "+hr+ "\n"+" בזמן: "+currentTimeStr();
-                findViewById(R.id.button3).setVisibility(View.INVISIBLE);
-                tv2.setVisibility(View.VISIBLE);
-                tv2.setText(ms);
-                (new Handler()).postDelayed(this::goHome, 5000);
+                addHeartRateToDB();
             }
-
-            private void goHome() {
-                Intent myIntent = new Intent(MainActivity5.this, MainActivity3.class);
-                MainActivity5.this.startActivity(myIntent);
-                }
         });
     }
 
@@ -94,5 +100,40 @@ public class MainActivity5 extends Activity implements SensorEventListener {
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+
+    public void addHeartRateToDB(){
+        String now = currentTimeStr();
+        Long nowMS = System.currentTimeMillis();
+
+        Map<String, Object> sample = new HashMap<>();
+        sample.put("heartRate", hr);
+        sample.put("dateTime", now);
+        sample.put("timeMS", nowMS);
+
+        db.collection("users").document(email)
+                .collection("notificationFB").document("list")
+                .set(sample)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        String ms = " נשלח דופק: "+hr+ "\n"+" בזמן: "+currentTimeStr();
+                        findViewById(R.id.button3).setVisibility(View.INVISIBLE);
+                        tv2.setVisibility(View.VISIBLE);
+                        tv2.setText(ms);
+                        (new Handler()).postDelayed(this::goHome, 5000);
+                        Log.d("Rubi", "DocumentSnapshot successfully written!");
+                    }
+                    private void goHome() {
+                        Intent myIntent = new Intent(MainActivity5.this, MainActivity3.class);
+                        MainActivity5.this.startActivity(myIntent);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("Rubi", "Error writing document", e);
+                    }
+                });
     }
 }
