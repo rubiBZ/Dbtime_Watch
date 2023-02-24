c5cbe803004147927fa88c24d01236db0a2229f2
Rubi Ben-Zvi <rubi.benzvi@gmail.com>
2023-02-24 09:45:47 +0200

    f

---
+++ app/src/main/java/com/example/Dbtime_Watch/Login.java
@@ -0,0 +1,16 @@
+package com.example.Dbtime_Watch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.Dbtime_Wathc.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
