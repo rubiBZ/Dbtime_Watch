debc6bcea852bcca470438f280f7532e572e41d2
Rubi Ben-Zvi <rubi.benzvi@gmail.com>
2023-02-24 09:47:14 +0200

    f

--- app/src/main/java/com/example/Dbtime_Watch/Login.java
+++ app/src/main/java/com/example/Dbtime_Watch/Login.java
@@ -2,11 +2,12 @@ package com.example.Dbtime_Watch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.example.Dbtime_Wathc.R;
 
-public class Login extends AppCompatActivity {
+public class Login extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
